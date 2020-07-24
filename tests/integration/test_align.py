0ef61206e5faf8847e026ab62119ea0253491ef3
Paul Sud <paulsud@stanford.edu>
2020-07-24 15:13:04 -0700

    update juicer and fix mapq0 sam

--- tests/integration/test_align.py
+++ tests/integration/test_align.py
@@ -11,7 +11,14 @@ def test_align_alignable_bam_match(workflow_dir, bam_md5):
 
 
 @pytest.mark.workflow(name="test_align")
-def test_align_mapq0_bam_match(workflow_dir, bam_md5):
+def test_align_unmapped_bam_match(workflow_dir, bam_md5):
     bam_path = workflow_dir / Path("test-output/unmapped.bam")
+    bam_md5sum = bam_md5(bam_path)
+    assert bam_md5sum == "e24ff1e0d5e6471fec6976d0aeaa1061"
+
+
+@pytest.mark.workflow(name="test_align")
+def test_align_mapq0_bam_match(workflow_dir, bam_md5):
+    bam_path = workflow_dir / Path("test-output/mapq0.bam")
     bam_md5sum = bam_md5(bam_path)
     assert bam_md5sum == "e24ff1e0d5e6471fec6976d0aeaa1061"
