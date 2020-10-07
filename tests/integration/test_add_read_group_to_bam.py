fe0bbd3e117d10862e8d6177e2fa2ec5add778cd
Paul Sud <paulsud@stanford.edu>
2020-10-07 15:27:01 -0700

    add quotes to unit test

---
+++ tests/integration/test_add_read_group_to_bam.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.mark.workflow("test_add_read_group_to_bam")
+def test_read_group_added_to_bam_header(workflow_dir):
+    # bam_path = workflow_dir / Path("test-output/merged_bam_files.bam")
+    # bam_md5sum = bam_md5(bam_path)
+    # assert bam_md5sum == "5e0726f291740a645f514e7a13ed7d21"
+    pass
