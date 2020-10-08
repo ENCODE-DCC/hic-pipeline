556d6cb54f6489e12e5b22ce08fddc5a80170495
Paul Sud <paulsud@stanford.edu>
2020-10-07 17:41:52 -0700

    fix bam path

--- tests/integration/test_add_read_group_to_bam.py
+++ tests/integration/test_add_read_group_to_bam.py
@@ -6,6 +6,6 @@ import pytest
 
 @pytest.mark.workflow("test_add_read_group_to_bam")
 def test_read_group_added_to_bam_header(workflow_dir):
-    bam_path = workflow_dir / Path("test-output/bam_with_read_group.bam")
+    bam_path = workflow_dir / Path("test-output/with_read_group.bam")
     with pysam.AlignmentFile(str(bam_path)) as bam:
         assert bam.header.get("RG")[0]["ID"] == "foo"
