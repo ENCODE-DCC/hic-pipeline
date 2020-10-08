a555edff7f76f04f9cd97b9b878d82555190c890
Paul Sud <paulsud@stanford.edu>
2020-10-08 11:27:13 -0700

    change test name

--- tests/integration/test_add_read_group_to_bam.py
+++ tests/integration/test_add_read_group_to_bam.py
@@ -10,12 +10,12 @@ def bam_path(workflow_dir):
 
 
 @pytest.mark.workflow("test_add_read_group_to_bam")
-def test_read_group_added_to_bam_header(workflow_dir, bam_path):
+def test_add_read_group_to_bam_read_group_added_to_bam_header(workflow_dir, bam_path):
     with pysam.AlignmentFile(str(bam_path)) as bam:
         assert bam.header.get("RG")[0]["ID"] == "foo"
 
 
 @pytest.mark.workflow("test_add_read_group_to_bam")
-def test_align_alignable_bam_match(workflow_dir, bam_path, bam_md5):
+def test_add_read_group_to_bam_bam_match(workflow_dir, bam_path, bam_md5):
     bam_md5sum = bam_md5(bam_path)
     assert bam_md5sum == "8cb6f293859775fdee81a51c73f51c1e"
