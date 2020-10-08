416ff5313d7554c1900bf09e953af0e7ad4b69d7
Paul Sud <paulsud@stanford.edu>
2020-10-08 13:55:04 -0700

    add another read group line to integration test

--- tests/integration/test_add_read_group_to_bam.py
+++ tests/integration/test_add_read_group_to_bam.py
@@ -13,6 +13,7 @@ def bam_path(workflow_dir):
 def test_add_read_group_to_bam_read_group_added_to_bam_header(workflow_dir, bam_path):
     with pysam.AlignmentFile(str(bam_path)) as bam:
         assert bam.header.get("RG")[0]["ID"] == "foo"
+        assert bam.header.get("RG")[0]["DS"] == "cool"
 
 
 @pytest.mark.workflow("test_add_read_group_to_bam")
