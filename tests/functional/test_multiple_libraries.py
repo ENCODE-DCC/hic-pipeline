feecc99def3bf648c053f455a5ae89a853ff5460
Paul Sud <paulsud@stanford.edu>
2021-04-26 18:29:51 -0700

    update md5s yet again

--- tests/functional/test_multiple_libraries.py
+++ tests/functional/test_multiple_libraries.py
@@ -9,4 +9,4 @@ def test_multiple_libraries_hic_match(workflow_dir):
         workflow_dir.glob("hic/*/call-create_hic/shard-1/execution/inter_30.hic")
     )
     hic_md5sum = hashlib.md5(hic_path.read_bytes()).hexdigest()
-    assert hic_md5sum == "d7186dc77cf8ff79c8efd4ba3ace1afb"
+    assert hic_md5sum == "034f2058ad43dfb08b9f95f09a5a3956"
