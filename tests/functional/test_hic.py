3e55d452f46a2e74d5b8eadc59635ebebbd1d1b7
Paul Sud <paulsud@stanford.edu>
2021-04-28 15:44:47 -0700

    update functional md5s

--- tests/functional/test_hic.py
+++ tests/functional/test_hic.py
@@ -7,7 +7,7 @@ import pytest
 def test_hic_merged_dedup_match(workflow_dir, bam_md5):
     bam_path = workflow_dir / Path("test-output/merged_dedup.bam")
     bam_md5sum = bam_md5(bam_path)
-    assert bam_md5sum == "6df22b9574332f4db46a03ee15fbd73a"
+    assert bam_md5sum == "bedd4a49ac87457775089d68b070c865"
 
 
 @pytest.mark.workflow("test_hic")
--- tests/functional/test_hic_with_read_group.py
+++ tests/functional/test_hic_with_read_group.py
@@ -15,7 +15,7 @@ def test_read_group_added_to_bam_header(workflow_dir):
 def test_hic_with_read_group_alignable_bam_match(workflow_dir, bam_md5):
     bam_path = workflow_dir / Path("test-output/merged_dedup.bam")
     bam_md5sum = bam_md5(bam_path)
-    assert bam_md5sum == "975d379867f06350b9121df91ffa181a"
+    assert bam_md5sum == "4de9898216a3fc817850d2e1459d3ccf"
 
 
 @pytest.mark.workflow("test_hic_with_read_group")
