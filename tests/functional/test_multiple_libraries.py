dbfb9d750d2c4733068be730ad29aed213aa7c40
Paul Sud <paulsud@stanford.edu>
2021-04-28 19:25:47 -0700

    fix path for glob

--- tests/functional/test_multiple_libraries.py
+++ tests/functional/test_multiple_libraries.py
@@ -6,7 +6,9 @@ import pytest
 @pytest.mark.workflow("test_multiple_libraries")
 def test_multiple_libraries_hic_match(workflow_dir):
     hic_path = next(
-        workflow_dir.glob("hic/*/call-create_hic/shard-1/execution/inter_30.hic")
+        workflow_dir.glob(
+            "hic/*/call-create_hic_with_chrom_sizes/shard-1/execution/inter_30.hic"
+        )
     )
     hic_md5sum = hashlib.md5(hic_path.read_bytes()).hexdigest()
     assert hic_md5sum == "0a65447aecd388044978367ce5e5ae97"
