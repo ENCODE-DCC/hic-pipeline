3b61b08a66eeb27021e4f1ae2e20ec6c145beeb3
Paul Sud <paulsud@stanford.edu>
2021-11-08 12:58:02 -0800

    fix tests

--- tests/functional/test_multiple_libraries.py
+++ tests/functional/test_multiple_libraries.py
@@ -11,4 +11,4 @@ def test_multiple_libraries_hic_match(workflow_dir):
         )
     )
     hic_md5sum = hashlib.md5(hic_path.read_bytes()).hexdigest()
-    assert hic_md5sum == "4aafbe27fec84119f2e2366e2f37b9e7"
+    assert hic_md5sum == "55d8c5e00d777c9494b7c08ca3164621"
