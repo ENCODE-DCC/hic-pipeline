16a30938370a2eae46673d242a8ca0a9def11521
Paul Sud <paulsud@stanford.edu>
2021-11-09 12:51:19 -0800

    remove pairs tests from py

--- tests/functional/test_hic_with_read_group.py
+++ tests/functional/test_hic_with_read_group.py
@@ -16,10 +16,3 @@ def test_hic_with_read_group_alignable_bam_match(workflow_dir, bam_md5):
     bam_path = workflow_dir / Path("test-output/merged_dedup.bam")
     bam_md5sum = bam_md5(bam_path)
     assert bam_md5sum == "4de9898216a3fc817850d2e1459d3ccf"
-
-
-@pytest.mark.workflow("test_hic_with_read_group")
-def test_hic_with_read_group_pairs_match(workflow_dir, skip_n_lines_md5):
-    pairs_path = workflow_dir / Path("test-output/pairix.bsorted.pairs.gz")
-    pairs_md5 = skip_n_lines_md5(pairs_path, n_lines=6)
-    assert pairs_md5 == "0e82a8e6db855a237506306e299d0d5f"
--- tests/functional/test_ultima.py
+++ tests/functional/test_ultima.py
@@ -8,10 +8,3 @@ def test_ultima_merged_dedup_match(workflow_dir, bam_md5):
     bam_path = workflow_dir / Path("test-output/merged_dedup.bam")
     bam_md5sum = bam_md5(bam_path)
     assert bam_md5sum == "59df19e09fc98126c3265984c07b0db5"
-
-
-# @pytest.mark.workflow("test_ultima")
-# def test_ultima_pairs_match(workflow_dir, skip_n_lines_md5):
-#     pairs_path = workflow_dir / Path("test-output/pairix.bsorted.pairs.gz")
-#     pairs_md5 = skip_n_lines_md5(pairs_path, n_lines=6)
-#     assert pairs_md5 == "d5e74f908559ccffa977f890565751c8"
