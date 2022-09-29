bdda733fe0e06debc9cf146af70112faf695e83a
Otto Jolanki <ojolanki@stanford.edu>
2022-09-29 16:24:58 -0700

    run tox in python3.9

--- scripts/make_genophase_input_json_from_portal.py
+++ scripts/make_genophase_input_json_from_portal.py
@@ -25,10 +25,7 @@ def main():
         auth=auth,
         use_submitted_file_names=args.use_submitted_file_names,
     )
-    donor_id = _get_donor_id_from_experiment(
-        accessions=args.accessions,
-        auth=auth
-    )
+    donor_id = _get_donor_id_from_experiment(accessions=args.accessions, auth=auth)
     input_json = _get_input_json(bams=bams, donor_id=donor_id)
     _write_json_to_file(input_json, args.outfile)
 
@@ -76,9 +73,16 @@ def _get_donor_id_from_experiment(accessions, auth=None):
     for accession in accessions:
         experiment = _get_experiment(accession, auth=auth)
         if donor_id is None:
-            donor_id = experiment["replicates"][0]["library"]["biosample"]["donor"]["accession"]
+            donor_id = experiment["replicates"][0]["library"]["biosample"]["donor"][
+                "accession"
+            ]
         else:
-            if donor_id != experiment["replicates"][0]["library"]["biosample"]["donor"]["accession"]:
+            if (
+                donor_id
+                != experiment["replicates"][0]["library"]["biosample"]["donor"][
+                    "accession"
+                ]
+            ):
                 raise ValueError("These datasets do not belong to the same donor.")
     return donor_id
 
@@ -88,7 +92,7 @@ def _get_input_json(bams, donor_id):
         "genophase.bams": bams,
         "genophase.gatk_bundle_tar": _REFERENCE_FILES["gatk_bundle_tar"],
         "genophase.reference_fasta": _REFERENCE_FILES["reference_fasta"],
-        "genophase.donor_id": donor_id
+        "genophase.donor_id": donor_id,
     }
     return input_json
 
