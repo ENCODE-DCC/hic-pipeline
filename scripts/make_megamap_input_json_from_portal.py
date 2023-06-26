677bbe03c98255f1ac9211a69692beb3019258df
ian-whaling <whaling.ian@gmail.com>
2023-06-25 19:03:20 -0700

    updated megamap script

--- scripts/make_megamap_input_json_from_portal.py
+++ scripts/make_megamap_input_json_from_portal.py
@@ -50,6 +50,7 @@ def _get_bigwig_files_and_hic_files_from_experiment(
             if (
                 a["status"] in _ALLOWED_STATUSES
                 and a["lab"] == "/labs/encode-processing-pipeline/"
+                and "/pipelines/ENCPL478DPO/" not in a["pipelines"]
             ):
                 analysis = a
                 break
@@ -57,14 +58,14 @@ def _get_bigwig_files_and_hic_files_from_experiment(
             raise ValueError("Could not find uniform processing pipeline analysis")
         for file in experiment["files"]:
             if file["status"] in _ALLOWED_STATUSES and file["@id"] in analysis["files"]:
-                if file["output_type"] == "DNA accessibility raw signal":
+                if file["output_type"] == "nuclease cleavage frequency":
                     if use_submitted_file_names:
                         bigwig_files.append(file["submitted_file_name"])
                     else:
                         bigwig_files.append(urljoin(_PORTAL_URL, file["href"]))
                 if (
                     file["output_type"]
-                    == "mapping quality thresholded chromatin interactions"
+                    == "mapping quality thresholded contact matrix"
                 ):
                     if use_submitted_file_names:
                         hic_files.append(file["submitted_file_name"])
